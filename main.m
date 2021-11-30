d8efae55217b5c534618509f228a1af16ab12ff8
mrf1989 <mruano@us.es>
2021-11-30 12:30:34 +0100

    v1.0 finished

--- main.m
+++ main.m
@@ -21,17 +21,25 @@ processed_audio = audio_filter(x, L, Fc, Fs);
 
 % Step 2.
 
-shift = 24000;
+shift = 34000;
 fq_shifted_audio = fq_shift(processed_audio, shift);
 audiowrite('fq_shifted_audio.wav', fq_shifted_audio, Fs);
 str = ['filtrado desplazado en frecuencia ' num2str(shift/1000) ' kHz'];
 audio_analysis('fq_shifted_audio.wav', str);
 
+%      O
+%      |
+%      |
+%      |
+% transmission
+%      |
+%      |
+%      |
+%      V
 
+% Step 3.
 
-
-
-% Reconstrucción de la señal original
-
-x2 = desplazamientoF(yd, -6000);
-sound(x2, Fs);
+reconstructed_audio = fq_shift(fq_shifted_audio, -shift);
+audiowrite('reconstructed_audio.wav', reconstructed_audio, Fs);
+str = ['reconstrucción del audio original filtrado'];
+audio_analysis('reconstructed_audio.wav', str);
